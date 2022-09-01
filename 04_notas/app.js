9d006d30982deeb7275b71373d46d50d01e5f0fb
carlos_zenbook <scarlosenrique@gmail.com>
2022-08-31 22:02:04 -0500

    subiendo notas_04 entregada

---
+++ 04_notas/app.js
@@ -0,0 +1,28 @@
+        // Datos iniciales
+        let alumnos = ['Juan Soto', 'Sofía Pérez', 'Miguel Nuñez', 'Pablo Silva'];
+        let registros = [];
+        let promedios = [];
+        let estados = {
+            "Aprobados": [],
+            "Desaprobados": []
+        };
+        
+        console.log(alumnos);
+        // Solicitar las notas por alumno
+        //Aquí va tu código:
+        alumnos.forEach(alumno => alert('ingrese la nota ${alumno}'));
+
+
+
+        console.log(registros);
+        
+        // Obtener los promedios de la lista de registros
+        //Aquí va tu código:
+
+
+        console.log(promedios);
+        
+        //Obtener la lista de cantidad de alumnos aprobados y desaprobados
+        //Aquí va tu código:
+        
+        console.log(estados);
